02dff34991e9496d40d8227288b6719845660dd2
skyboy <mr.wraith.06@gmail.com>
2016-02-14 20:50:22 -0500

    Mark Beta

--- src/cofh/tweak/CoFHTweaks.java
+++ src/cofh/tweak/CoFHTweaks.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.LoaderState;
 
 public class CoFHTweaks {
 
-	public static final String version = "1.7.10R1.1.2";
+	public static final String version = "1.7.10R1.1.3B1";
 
 	public static boolean canHaveWorld() {
 
